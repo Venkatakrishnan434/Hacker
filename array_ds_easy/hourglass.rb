385f3c611c0f78aa3acc1ef92cf75aa3e7ac8c08
venkatakrishnan434 <venkatakrishnan@434@gmail.com>
2022-09-16 12:48:44 +0530

    added new files

--- array_ds_easy/hourglass.rb
+++ array_ds_easy/hourglass.rb
@@ -1,16 +1,16 @@
 # Complete the function hourglassSum in the editor below.
 
-def hourglassSum(arr)
+def hourglassSum(hour)
    
     temp = []
     4.times do |i|
         4.times do |j|
-             temp << arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+             temp << hour[i][j] + hour[i][j+1] + hour[i][j+2] + hour[i+1][j+1] + hour[i+2][j] + hour[i+2][j+1] + hour[i+2][j+2]
         end
     end
     temp.max
 end
 
-params_array = [1,2,3,4,5,6]
-puts hourglassSum([params_array, params_array, params_array, params_array, params_array,params_array])
+arr = [1,2,3,4,5,6]
+puts hourglassSum([arr, arr, arr, arr, arr,arr])
 
